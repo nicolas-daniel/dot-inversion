6904f369dd557d17f95779585719c5d9f0f1136c
nicolas-daniel <nicolas.daniel.29@gmail.com>
2015-02-22 14:25:33 +0100

    play animations with arrows

--- js/dot.js
+++ js/dot.js
@@ -19,6 +19,8 @@
 	 */
 	p.init = function() {
 		p.initPixi();
+
+		document.addEventListener('keydown', p.playAnimation);
 	};
 
 	/**
@@ -32,10 +34,37 @@
 
 		p.initBackground();
 		p.initDot();
-		p.initTimeline();
-		p.tl.play();
+		p.initTimelines();
 		
 		requestAnimFrame(p.animate);
+	};
+
+	/**
+	 * Play animation
+	 */
+	p.playAnimation = function(e) {
+		if ( !p.isAnimated ) {
+			if ( e.keyCode == 37 ) {
+				p.isAnimated = true;
+				p.iteration++;
+				p.tlWest.restart();
+			}
+			if ( e.keyCode == 38 ) {
+				p.isAnimated = true;
+				p.iteration++;
+				p.tlNorth.restart();
+			}
+			if ( e.keyCode == 39 ) {
+				p.isAnimated = true;
+				p.iteration++;
+				p.tlEast.restart();
+			}
+			if ( e.keyCode == 40 ) {
+				p.isAnimated = true;
+				p.iteration++;
+				p.tlSouth.restart();
+			}
+		}
 	};
 
 	/**
@@ -72,25 +101,77 @@
 	};
 
 	/**
+	 * Init timelines
+	 */
+	p.initTimelines = function() {
+		p.initEastTimeline();
+		p.initWestTimeline();
+		p.initNorthTimeline();
+		p.initSouthTimeline();
+	};
+
+	/**
+	 * Init East timeline
+	 */
+	p.initEastTimeline = function() {
+		p.tlEast = new TimelineMax({paused: true, onRepeat: p.onAnimationRepeat, onComplete: function(){
+			p.isAnimated = false;
+		}});
+		p.initTimeline(p.tlEast, 'x', 1);
+	};
+
+	/**
+	 * Init West timeline
+	 */
+	p.initWestTimeline = function() {
+		p.tlWest = new TimelineMax({paused: true, onRepeat: p.onAnimationRepeat, onComplete: function(){
+			p.isAnimated = false;
+		}});
+		p.initTimeline(p.tlWest, 'x', -1);
+	};
+
+	/**
+	 * Init North timeline
+	 */
+	p.initNorthTimeline = function() {
+		p.tlNorth = new TimelineMax({paused: true, onRepeat: p.onAnimationRepeat, onComplete: function(){
+			p.isAnimated = false;
+		}});
+		p.initTimeline(p.tlNorth, 'y', -1);
+	};
+
+	/**
+	 * Init South timeline
+	 */
+	p.initSouthTimeline = function() {
+		p.tlSouth = new TimelineMax({paused: true, onRepeat: p.onAnimationRepeat, onComplete: function(){
+			p.isAnimated = false;
+		}});
+		p.initTimeline(p.tlSouth, 'y', 1);
+	};
+
+	/**
 	 * Timeline initialization
 	 */
-	p.initTimeline = function() {
-		p.tl = new TimelineMax({paused:true, delay: 1, repeat: -1, onRepeat: onAnimationRepeat});
-		p.tl.to(p.dot.position, 1, {x: 25*20, ease: Expo.easeIn});
-		p.tl.to(p.dot.scale, 0.5, {x: 20, y: 20, ease: Expo.easeIn, onComplete: changeSideRightColor}, 0.5);
-		p.tl.set(p.dot.position, {x: -(p.dot.width*20)/2, ease: Expo.ease, onComplete: changeColors});
-		p.tl.to(p.dot.scale, 0.5, {x: 1, y: 1, ease: Expo.easeOut}, 1);
-		p.tl.to(p.dot.position, 1, {x: 0, ease: Expo.easeOut}, 1);
+	p.initTimeline = function(tl, axe, direction) {
+		if ( axe == 'x' ) tl.to(p.dot.position, 1, {x: direction*25*20, ease: Expo.easeIn});
+		else tl.to(p.dot.position, 1, {y: direction*25*20, ease: Expo.easeIn});
+		tl.to(p.dot.scale, 0.5, {x: 20, y: 20, ease: Expo.easeIn, onComplete: changeSideRightColor}, 0.5);
+		if ( axe == 'x' ) tl.set(p.dot.position, {x: -direction*(p.dot.width*20)/2, ease: Expo.ease, onComplete: changeColors});
+		else tl.set(p.dot.position, {y: -direction*(p.dot.width*20)/2, ease: Expo.ease, onComplete: changeColors});
+		tl.to(p.dot.scale, 0.5, {x: 1, y: 1, ease: Expo.easeOut}, 1);
+		if ( axe == 'x' ) tl.to(p.dot.position, 1, {x: 0, ease: Expo.easeOut}, 1);
+		else tl.to(p.dot.position, 1, {y: 0, ease: Expo.easeOut}, 1);
 
 		function changeSideRightColor() {
 			if ( p.iteration%2 == 0 ) {
 				p.sideRight.clear();
-				p.sideRight.beginFill(p.colors.indigo, 1);
+				p.sideRight.beginFill(p.colors.white, 1);
 				p.sideRight.drawRect(window.innerWidth/2, 0, window.innerWidth/2, window.innerHeight);
 				p.sideContainer.addChild(p.sideRight);
 			} else {
 				p.sideRight.clear();
-				p.sideRight.beginFill(p.colors.white, 1);
+				p.sideRight.beginFill(p.colors.indigo, 1);
 				p.sideRight.drawRect(window.innerWidth/2, 0, window.innerWidth/2, window.innerHeight);
 				p.sideContainer.addChild(p.sideRight);
 			}
@@ -98,16 +179,6 @@
 
 		function changeColors() {
 			if ( p.iteration%2 == 0 ) {
-				p.dot.clear();
-				p.dot.beginFill(0xF0F0F0, 1);
-				p.dot.drawCircle(0, 0, 25);
-				p.dotContainer.addChild(p.dot);
-
-				p.sideLeft.clear();
-				p.sideLeft.beginFill(0x3F51B5, 1);
-				p.sideLeft.drawRect(0, 0, window.innerWidth/2, window.innerHeight);
-				p.sideContainer.addChild(p.sideLeft);
-			} else {
 				p.dot.clear();
 				p.dot.beginFill(0x3F51B5, 1);
 				p.dot.drawCircle(0, 0, 25);
@@ -117,12 +188,25 @@
 				p.sideLeft.beginFill(0xF0F0F0, 1);
 				p.sideLeft.drawRect(0, 0, window.innerWidth/2, window.innerHeight);
 				p.sideContainer.addChild(p.sideLeft);
+			} else {
+				p.dot.clear();
+				p.dot.beginFill(0xF0F0F0, 1);
+				p.dot.drawCircle(0, 0, 25);
+				p.dotContainer.addChild(p.dot);
+
+				p.sideLeft.clear();
+				p.sideLeft.beginFill(0x3F51B5, 1);
+				p.sideLeft.drawRect(0, 0, window.innerWidth/2, window.innerHeight);
+				p.sideContainer.addChild(p.sideLeft);
 			}
 		}
+	};
 
-		function onAnimationRepeat() {
-			p.iteration++;
-		}
+	/**
+	 * Update animation iteration count
+	 */	
+	p.onAnimationRepeat = function() {
+		p.iteration++;
 	};
 
 	/**
